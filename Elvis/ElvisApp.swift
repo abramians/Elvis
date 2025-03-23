db45ba4b7c3a6bf10de5b6378b6e81fdbdc8b705
Armen Melik-Abramians <abramians2110@gmail.com>
2025-03-23 11:04:30 -0700

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Armen Melik-Abramians on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
